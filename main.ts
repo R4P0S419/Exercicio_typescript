f2dce3a0e509552c8f4007ab465bd6c0fe01255a
Raposa Foxyghostgm <r4p0s419@gmail.com>
2024-05-08 20:52:58 -0300

    first commit

---
+++ main.ts
@@ -0,0 +1,11 @@
+
+function multiplicacao(num1: number, num2: number): number {
+    return num1 * num2;
+}
+
+function saudacao(nome: string): string {
+    return "Olá " + nome;
+}
+
+console.log(multiplicacao(4, 5)); // Saída: 20
+console.log(saudacao("Kaique")); // Saída: Olá Kaique
